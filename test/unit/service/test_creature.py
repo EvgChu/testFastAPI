c262cabe1ba67f21fe7163a1513a7610e275d53b
EvgChu <chuevgenii@gmail.com>
2024-07-29 20:32:14 +0300

    add tests for service

---
+++ test/unit/service/test_creature.py
@@ -0,0 +1,28 @@
+from src.model.creature import Creature
+from src.service import creature as code
+
+
+sample = Creature(
+    name="Goblin",
+    aka="aka Goblin",
+    country="CN",
+    area="Himalayes",
+    description="Goblin is a very dangerous creature.",
+)
+
+
+def test_create():
+    resp = code.create(sample)
+    assert resp == sample
+   
+    
+def test_get_exists():
+    resp = code.get_one(sample.name)
+    assert resp == sample
+  
+    
+def test_get_missing():
+    resp = code.get_one("abra")
+    assert resp is None
+    
+print("Ddd")
