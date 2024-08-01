4491e3455c0ebbc66a38534b2be79e2e371b3199
EvgChu <chuevgenii@gmail.com>
2024-08-01 22:44:15 +0300

    add forwarded_allow_ips for run uvicorn

--- src/main.py
+++ src/main.py
@@ -13,4 +13,10 @@ def echo(thing):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=True,
+        forwarded_allow_ips="*",
+    )
